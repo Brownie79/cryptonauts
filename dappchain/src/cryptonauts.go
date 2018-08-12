394236fe978a0762256261695516b46eb10157d3
Dev Bharel <dev@spacemanholdings.com>
2018-08-11 21:38:20 -0500

    structs defined

--- dappchain/src/cryptonauts.go
+++ dappchain/src/cryptonauts.go
@@ -9,7 +9,51 @@ func main() {
 	plugin.Serve(Contract)
 }
 
-type Cryptonauts struct {
+type Cryptonauts struct{}
+type Building struct {
+	name      string
+	level     int
+	resType   string // "Food" "Metal" "Coin" "Research" "Essence"
+	resValue  int
+	hitpoints int
+}
+type Turret struct {
+	hitpoints int
+	damage    int
+}
+type Planet struct {
+	name      string //will be the blockhash in which it was discovered
+	coords    string // (0,0) is Earth
+	buildings []Building
+	turrets   []Turret
+}
+type Resources struct {
+	resFood     int
+	resMetal    int
+	resCoin     int
+	resResearch int
+}
+type GameState struct {
+	planets   []Planet
+	resources Resources
+}
+type Upgrade struct{}
+type Hero struct {
+	level     int
+	hitpoints int
+	defense   int
+	speed     int
+	dps       int
+	cost      int
+}
+
+type Player struct {
+	privatekey  string
+	essence     int
+	currentGame GameState
+	pastGames   []GameState
+	inventory   []Upgrade
+	heroes      []Hero
 }
 
 func (e *Cryptonauts) Meta() (plugin.Meta, error) {
